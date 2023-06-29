937eaa498e31652928d2ec2aad6b8bb912cb2b74
Bob Means (BoDonkey) <robert@apostrophecms.com>
2023-06-29 06:16:52 -0400

    commit all tutorial change

--- modules/@apostrophecms/home-page/index.js
+++ modules/@apostrophecms/home-page/index.js
@@ -4,10 +4,17 @@ module.exports = {
   },
   fields: {
     add: {
-      main: {
+      topArea: {
         type: 'area',
+        label: 'Top Area',
         options: {
           widgets: {
+            '@apostrophecms/image': {
+              // This option will add these classes to any image
+              className: 'img-fluid image-fit-center-center'
+            },
+            '@apostrophecms/video': {},
+            '@apostrophecms/html': {},
             '@apostrophecms/rich-text': {
               toolbar: [
                 'styles',
@@ -18,12 +25,30 @@ module.exports = {
                 'link',
                 '|',
                 'bulletList',
-                'orderedList'
+                'orderedList',
+                '|',
+                'blockquote',
+                'codeBlock',
+                '|',
+                'horizontalRule',
+                '|',
+                'undo',
+                'redo'
               ],
+              insert: ['table', 'image'],
               styles: [
                 {
                   tag: 'p',
                   label: 'Paragraph (P)'
+                },
+                {
+                  tag: 'h2',
+                  label: 'Heading 2 (H2)'
+                },
+                {
+                  tag: 'h2',
+                  label: 'Special underline',
+                  class: 'custom-underline display-6 ps-2'
                 },
                 {
                   tag: 'h3',
@@ -34,9 +59,72 @@ module.exports = {
                   label: 'Heading 4 (H4)'
                 }
               ]
-            },
-            '@apostrophecms/image': {},
-            '@apostrophecms/video': {}
+            }
+          }
+        }
+      },
+      bottomArea: {
+        type: 'area',
+        label: 'Bottom Area',
+        options: {
+          expanded: true,
+          groups: {
+            core: {
+              label: 'Core Widgets',
+              widgets: {
+                '@apostrophecms/image': {
+                  // This option will add these classes to any image
+                  className: 'img-fluid image-fit-center-center'
+                },
+                '@apostrophecms/video': {},
+                '@apostrophecms/html': {},
+                '@apostrophecms/rich-text': {
+                  toolbar: [
+                    'styles',
+                    '|',
+                    'bold',
+                    'italic',
+                    'strike',
+                    'link',
+                    '|',
+                    'bullet_list',
+                    'ordered_list',
+                    '|',
+                    'blockquote',
+                    'code_block',
+                    '|',
+                    'horizontal_rule',
+                    '|',
+                    'undo',
+                    'redo'
+                  ],
+                  styles: [
+                    {
+                      tag: 'p',
+                      label: 'Paragraph (P)'
+                    },
+                    {
+                      tag: 'h2',
+                      label: 'Heading 2 (H2)'
+                    },
+                    {
+                      tag: 'h2',
+                      label: 'Special underline',
+                      class: 'custom-underline display-6 ps-2'
+                    },
+                    {
+                      tag: 'h3',
+                      label: 'Heading 3 (H3)'
+                    },
+                    {
+                      tag: 'h4',
+                      label: 'Heading 4 (H4)'
+                    }
+                  ]
+                }
+              },
+              columns: 2
+            }
           }
         }
       }
@@ -44,10 +132,7 @@ module.exports = {
     group: {
       basics: {
         label: 'Basics',
-        fields: [
-          'title',
-          'main'
-        ]
+        fields: [ 'topArea', 'bottomArea' ]
       }
     }
   }
