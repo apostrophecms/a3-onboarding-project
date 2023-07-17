f52ffe4923a035a4a0d75d4bd3b64554b0ed58ad
Bob Means (BoDonkey) <robert@apostrophecms.com>
2023-07-17 05:32:42 -0400

    initial pieces section

---
+++ modules/review-page/index.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extend: '@apostrophecms/piece-page-type',
+  options: {
+    label: 'Review Page',
+    pluralLabel: 'Review Pages',
+  },
+  fields: {
+    add: {},
+    group: {}
+  }
+};
