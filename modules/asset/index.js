a18647fdb0edd4625585ee012edae28ed486c50f
Bob Means (BoDonkey) <robert@apostrophecms.com>
2023-08-02 09:18:51 -0400

    Code changes for the navigation tutorial

--- modules/asset/index.js
+++ modules/asset/index.js
@@ -1,20 +1,10 @@
 module.exports = {
-  // options: {
-  //   alias: 'helper'
-  // },
   handlers(self) {
     return {
       '@apostrophecms/page:beforeSend': {
         webpack(req) {
           req.data.isDev = (process.env.NODE_ENV !== 'production');
         }
-      }
-    };
-  },
-  helpers(self) {
-    return {
-      debugMode() {
-        return process.env.DEBUG === 'true';
       }
     };
   }
