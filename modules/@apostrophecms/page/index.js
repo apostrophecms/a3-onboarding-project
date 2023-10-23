98b9357a3f777a36c4c4aa088f6db52730039c79
Bob Means (BoDonkey) <robert@apostrophecms.com>
2023-10-23 11:32:32 -0400

    Update for extension installation changes

--- modules/@apostrophecms/page/index.js
+++ modules/@apostrophecms/page/index.js
@@ -15,6 +15,10 @@ module.exports = {
       {
         name: 'review-page',
         label: 'Category Pages'
+      },
+      {
+        name: 'blog-page',
+        label: 'Blog'
       }
     ]
   }
