9573097bf6b703f5215f04f9cb231cb95a676b12
Bob Means (BoDonkey) <robert@apostrophecms.com>
2023-10-25 14:40:40 -0400

    reconcile ratings

--- modules/review/index.js
+++ modules/review/index.js
@@ -31,7 +31,7 @@ module.exports = {
           widgets: {
             '@apostrophecms/rich-text': {},
             '@apostrophecms/image': {},
-            ratings: {}
+            rating: {}
           }
         }
       },
